844d0b6cb74b680e2b5b01ed6b848195f207f572
HarrisonStrand <harrisonstrand@gmail.com>
2021-07-27 18:17:57 -0700

    deployment for heroku

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+	res.send('Hello to CoLAB API');
+})
+
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
